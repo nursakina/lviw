e8e60001c69b7be01ca98b0a951fd13014b3c568
nursakina <nursakina611@gmail.com>
2022-02-23 23:58:52 +0800

    malas

--- day1.py
+++ day1.py
@@ -1,1 +1,21 @@
+from telnetlib import theNULL
+
+
 print("Hello world")
+nama_depan ='nur'
+nama_belakang ='sakina' 
+nama_lengkap = nama_depan + nama_belakang
+print(nama_lengkap)
+
+
+print('-'*100)
+nama = 'sakina'
+print(nama)
+kelas = 'informatika e'
+print(kelas)
+umur = '19 tahun'
+print(umur)
+angkatan = '21'
+print(angkatan)
+nim ='D0221334'
+print(nim)
--- day2.py
+++ day2.py
@@ -1,2 +1,8 @@
 for i in range(1,10):
     print(i)
+
+print('-'*100)
+
+for i in range (1,100) :
+    if i%2 ==0 :
+        print(i)
