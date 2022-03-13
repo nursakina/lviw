fcec398ea391360fb57fcb17bf49fd9d4c5b19ed
KINO <100189010+nursakina@users.noreply.github.com>
2022-03-13 21:19:04 +0800

    tugas SDA

---
+++ variabel dan tipe data.py
@@ -0,0 +1,19 @@
+# tipe data string (karakter)
+nama = "nursakina"
+print(nama)
+print(type(nama))
+
+#tipe data integer (berisikan angka bilangan bulat)
+umur = 18
+print(umur)
+print(type(umur))
+
+# tipe data float (beriskan angka koma atau bilangna desimal)
+tinggi_badan = 1.45
+print(tinggi_badan)
+print(type(tinggi_badan))
+
+# tipe data boolean ( false and True)
+mahasiswa = True 
+print(mahasiswa)
+print(type(mahasiswa))
