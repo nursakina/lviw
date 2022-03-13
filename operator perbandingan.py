0c8d1aacb8433e862d8f43631f87c6681238efb0
KINO <100189010+nursakina@users.noreply.github.com>
2022-03-13 21:20:41 +0800

    tugas SDA

---
+++ operator perbandingan.py
@@ -0,0 +1,16 @@
+#OPERATOR PERBANDINGAN
+
+#lebih besar sama dengan >=
+# lebih kecil sama dengan <=
+# lebih besar dari >
+# lebih kecil dari <
+# sama dengan ==
+# tidak  sama dengan !=
+
+print(2 >= 4)
+print(2 <= 4)
+print(2 > 4)
+print(2 < 4)
+print(2 == 4)
+print(2 != 4)
+
