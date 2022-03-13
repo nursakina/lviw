d8f2e80251dc78a163a0b826270a28a1dcef88d5
KINO <100189010+nursakina@users.noreply.github.com>
2022-03-13 21:21:18 +0800

    tugas SDA

---
+++ percabgan.py
@@ -0,0 +1,25 @@
+x = 10
+if (x >7 ):
+    print("benar")
+
+#if else
+x = 10
+if (x <12 ):
+    print("salah")
+else:
+    print("benar")
+
+#elif
+nilai = 40
+if (nilai >=80 and nilai <= 100):
+    print("predikat 1")
+elif (nilai >=65 and nilai <= 79):
+    print("predikat 2")
+elif (nilai >=68 and nilai <= 90):
+    print("predikat 3")
+elif (nilai >= 90 and nilai <= 100):
+    print("predikat 4")
+else :
+    print("predikat 5")
+
+
