7c327b66f06baf829ab5cc0879b49f391a03a478
KINO <100189010+nursakina@users.noreply.github.com>
2022-03-14 07:34:38 +0800

    tugas SDA

---
+++ Operator penugasan.py
@@ -0,0 +1,34 @@
+# sama dengan =
+# tambah sama dengan +=
+# kurang sama dengan -=
+# kali sama dengan *=
+# bagi sama dengan /=
+# sisa bagi sama dengan %=
+# pangkat sama dengan **=
+# pembagian bilangan bulat //=
+
+nilai = 6
+nilai2 = 12
+
+nilai += 3 #(nilai +=2) sama dengan ( nilai = nilai + 2)
+nilai2 += 3 # nilai2 =+2 sama dengan nilai2 = 3
+print(nilai)
+print(nilai2)
+
+nilai -=3
+print(nilai)
+
+nilai *=6
+print(nilai)
+
+nilai /= 5
+print(nilai)
+
+nilai %= 4
+print(nilai)
+
+nilai **= 2
+print(nilai)
+
+nilai//= 8
+print(nilai)
